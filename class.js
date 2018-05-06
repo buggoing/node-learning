8dead38d48af3bbdb3ad80dce5e8d7672596dd6d
buggoing <onbug@qq.com>
2018-05-06 13:35:01 +0800

    add class sample

--- class.js
+++ class.js
@@ -11,5 +11,39 @@ class Test {
     }
 }
 
+class SubTest extends Test {
+    constructor(...args) {
+        super(...args);
+    }
+    sayGoodBye() {
+        log('Bye Bye!');
+    }
+}
+
+class Collection {
+    constructor() {
+        this.items = [];
+    }
+
+    *[Symbol.iterator]() {
+        for (let x of this.items) {
+            yield x;
+        }
+    }
+}
+
+
 let test = new Test(11);
+let test1 = new Test(12);
 test.sayHello();
+console.log(test.sayHello === test1.sayHello);
+
+let sub1 = new SubTest(24,45);
+sub1.sayGoodBye()
+console.log(test.sayHello === sub1.sayHello)
+
+let coll = new Collection();
+coll.items.push(23,45,5);
+for (let x of coll) {
+    console.log(x);
+}
